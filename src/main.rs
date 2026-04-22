e0895419df315a167033889c24c3180e125c1f19
peter2500zz <2797880090@qq.com>
2026-04-22 22:26:37 +0800

    从参数读取图片绘制，减少刷新率

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,38 @@
+use std::env;
 use std::num::NonZeroU32;
+use std::path::PathBuf;
 use std::rc::Rc;
 
+use anyhow::Result;
+use image::{DynamicImage, GenericImageView};
 use softbuffer::{Context, Surface};
 use winit::application::ApplicationHandler;
+use winit::dpi::LogicalSize;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, EventLoop, OwnedDisplayHandle};
 use winit::window::{Window, WindowId};
 
 // Your app state — owns windows, renderers, etc.
 struct App {
+    // images: Vec<PathBuf>,
+    current_image: DynamicImage,
+
     context: Context<OwnedDisplayHandle>,
     state: Option<AppState>,
 }
 
 struct AppState {
-    window: Rc<Window>,
+    // window: Rc<Window>,
     surface: Surface<OwnedDisplayHandle, Rc<Window>>,
 }
 
 impl ApplicationHandler for App {
     // Platform signals ready — create windows here
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let (w, h) = self.current_image.dimensions();
+
         let attrs = Window::default_attributes()
+            .with_inner_size(LogicalSize::new(w, h))
             .with_decorations(false)
             .with_title("Buzzer");
 
@@ -37,7 +48,10 @@ impl ApplicationHandler for App {
             surface.resize(width, height).unwrap();
         }
 
-        self.state = Some(AppState { window, surface });
+        self.state = Some(AppState { 
+            // window, 
+            surface 
+        });
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
@@ -45,13 +59,18 @@ impl ApplicationHandler for App {
             WindowEvent::CloseRequested => event_loop.exit(),
             WindowEvent::RedrawRequested => {
                 // render here
-                self.state.as_ref().unwrap().window.request_redraw();
+                // self.state.as_ref().unwrap().window.request_redraw();
 
                 let surface = &mut self.state.as_mut().unwrap().surface;
 
                 let mut buffer = surface.buffer_mut().unwrap();
 
-                buffer.fill(0xFFF8CEF0);
+                let rgba = self.current_image.to_rgba8();
+
+                for (dst, src) in buffer.iter_mut().zip(rgba.pixels()) {
+                    let [r, g, b, _a] = src.0;
+                    *dst = ((r as u32) << 16) | ((g as u32) << 8) | (b as u32);
+                }
 
                 buffer.present().unwrap();
             }
@@ -60,11 +79,26 @@ impl ApplicationHandler for App {
     }
 }
 
-fn main() {
+fn main() -> Result<()> {
+    let mut args: Vec<PathBuf> = env::args().map(PathBuf::from).collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: buzzer <image1> <image2> ...");
+        std::process::exit(1);
+    }
+
+    args.remove(0); // Remove the program name
+
+    let first_image = image::open(&args[0])?;
+
     let event_loop = EventLoop::new().unwrap();
     let mut app = App {
+        // images: args,
+        current_image: first_image,
         context: Context::new(event_loop.owned_display_handle()).unwrap(),
         state: None,
     };
     event_loop.run_app(&mut app).unwrap();
+
+    Ok(())
 }
